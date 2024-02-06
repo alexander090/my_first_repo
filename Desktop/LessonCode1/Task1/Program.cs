bff4dc74a231ddaa74637854c91c37dbf9b32319
Alexander <sashakormanovskiy@gmail.com>
2024-02-06 12:07:42 +0100

    Add new variables lesson1:Task1

--- Desktop/LessonCode1/Task1/Program.cs
+++ Desktop/LessonCode1/Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("hello World");
+
+int value = 100; 
+Console.WriteLine(value);
+
+double d = 5.55;
+
+char c = '!';
+bool b = true;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+
+
+
