c5a583c9126b5a593dca2b69e1287207989ded00
Alexander <sashakormanovskiy@gmail.com>
2024-02-20 15:51:51 +0100

    Complited Task1 with N and M numbers

---
+++ Desktop/Github/control/Task1/Esercizio/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Due(int m, int n)
+{
+if (m > n)
+return;
+if (m % 2 == 0)
+{
+Console.Write($"{m}, ");
+}
+Due(m+1,n);
+
+}
+Due(m,n);
+
